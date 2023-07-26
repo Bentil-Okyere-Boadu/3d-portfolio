102a5195c4c9dfc918a9f7182ad08e8679eb56c4
Bentil-Okyere-Boadu <bokyereboadu@gmail.com>
2023-07-26 13:59:48 +0000

    cleanup: unused imports

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import { navLinks } from "../constants";
 import { Link } from "react-router-dom";
 import { styles } from "../styles";
